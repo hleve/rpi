d6d235a78ef5a47965d8d745c44f03e0afb489f0
hleve <levesquehenry@gmail.com>
2024-10-02 23:15:05 -0400

    gps serial0

--- main.py
+++ main.py
@@ -18,11 +18,10 @@ print("Enabled Camera")
 
 
 # Setup GPS communication
-try:
-    gps_serial = serial.Serial("/dev/serial0", baudrate=9600, timeout=1)
-    print("camera port opened successfully")
-except serial.SerialException as e:
-    print(e)
+
+gps_serial = serial.Serial("/dev/serial0", baudrate=9600, timeout=1)
+print("camera port opened successfully")
+
 
 # Data storage
 data = []
